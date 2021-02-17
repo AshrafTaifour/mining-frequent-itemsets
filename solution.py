eb47c4a7935db5abe4d9e21652d2cee18a7e207b
AshrafTaifour <60125273+AshrafTaifour@users.noreply.github.com>
2021-02-16 23:43:59 -0500

    added count of singeltons

--- solution.py
+++ solution.py
@@ -1,15 +1,36 @@
 
 filepath = "retail.txt"
 basketsContainer = []
-basketsTable = {}
+
 with open(filepath, "r") as fp:
     line = fp.readline()
     while line:
+        #removes new line at end
         line = line.strip('\n')
+        #splits every number in the basket
+        line = line.split()
         basketsContainer.append(line)
         line = fp.readline()
 
 
 
-print(basketsContainer)
-print(len(basketsContainer))
+
+#print(basketsContainer)
+#print(len(basketsContainer))
+
+def firstPass(lst: list) -> dict:
+    basketsTable = {}
+    #select a basket
+    for i in lst:
+        for j in i:
+            if j not in basketsTable:
+                basketsTable[j] = 1
+            else:
+                basketsTable[j] += 1
+            
+
+    return basketsTable
+
+
+dct = firstPass(basketsContainer)
+print(dct)
